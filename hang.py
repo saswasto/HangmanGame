eb6f01562a5bcfc27d70387f28b77a7f9b916322
Sasoto Basak <89937629+saswasto@users.noreply.github.com>
2023-08-11 19:48:43 +0530

    Create hang.py

---
+++ hang.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+print("           **----HANGMAN----**     ")
+print("\nWelcome to Hangman game by Saswato and Raima \n")
+name = input("Enter your name: ")
+print("Hello " + name + "\n! Best of Luck!")
+time.sleep(2)
+print("The game is about to start!\n Let's play Hangman!")
+time.sleep(3)
+
+print("Rules: you have to choose 1 letter at a time .Not more than 1 letter you can choose")
+time.sleep(4)
